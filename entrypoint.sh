bdbe6b1eceb103a60d3250e29a3b75f74d478192
ubc-tuehoang <86985864+ubc-tuehoang@users.noreply.github.com>
2022-12-11 11:51:28 -0800

    udpated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/sh -l
 
-echo "::debug ::Debug Message"
-echo "::warning ::Warning Message"
-echo "::error ::Error Message"
-
-echo "::add-mask: $1"
-
-echo "GH-Actions-Docker-On-Demand: $1"
-
+echo "Hello $1"
 time=$(date)
-echo "::set-output name=time::$time"
-
-echo '::set-env name=ENV-VAR1::env-var1-WHATEVER'
 echo "time=$time" >> $GITHUB_OUTPUT
